3c4f9fdf3adf2a69e7f6f69a2739daf7e456d1b9
Daniil Pavlovich <layred.dota2@mail.ru>
2026-02-16 18:30:42 +0300

    feat(bingx): :sparkles: add get_account_uid method to SubMixin
    - Implemented get_account_uid method for querying the Account UID from the BingX API.
    - Added detailed docstring with example response for clarity on usage and expected output.

--- aiotrade/clients/bingx/_mixins/_account/_sub.py
+++ aiotrade/clients/bingx/_mixins/_account/_sub.py
@@ -19,3 +19,27 @@ class SubMixin:
         https://bingx-api.github.io/docs-v3/#/en/Account%20and%20Wallet/Sub-account%20Management/%20Query%20API%20KEY%20Permissions
         """
         return await self.get("/openApi/v1/account/apiPermissions", auth=True)
+
+    async def get_account_uid(
+        self: HttpClientProtocol,
+    ) -> Dict[str, Any]:
+        """
+        Query the Account UID.
+
+        GET /openApi/account/v1/uid
+
+        https://bingx-api.github.io/docs-v3/#/en/Account%20and%20Wallet/Sub-account%20Management/Query%20Account%20UID
+
+        Returns:
+            Dict[str, Any]: Response containing the account UID and related info.
+
+        Example response:
+            {
+                "code": 0,
+                "timestamp": 1702558965648,
+                "data": {
+                    "uid": 16844999
+                }
+            }
+        """
+        return await self.get("/openApi/account/v1/uid", auth=True)
